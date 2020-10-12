b226d68f4b61a146838c905989d29c2495de4f95
SilentNN <akanam@gmail.com>
2020-10-11 23:05:36 -0700

    GraphNode#initialize

---
+++ W4D1/graph_node.rb
@@ -0,0 +1,11 @@
+class GraphNode
+    attr_accessor :neighbors
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+    def bfs(start, target)
+        
+    end
+end
