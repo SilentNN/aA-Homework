c9d0c459c26dd062502a43cce8e32c31a8c9cf72
SilentNN <akanam@gmail.com>
2020-10-21 10:16:40 -0700

    w5d3 hw

--- W5D3/plays_playwrights_orm/plays.rb
+++ W5D3/plays_playwrights_orm/plays.rb
@@ -48,4 +48,65 @@ class Play
         id = ?
     SQL
   end
+
+  def self.find_by_title(title)
+    Play.all.each { |play| return play if play.title == title }
+  end
+
+  def self.find_by_playwright(name)
+    output = []
+    playw_id = PlayWright.find_by_name(name).id
+    Play.all.each { |play| output << play if play.playwright_id == playw_id}
+    output
+  end
+
+
+end
+
+class PlayWright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| PlayWright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    PlayWright.all.each { |playwright| return playwright if playwright.name == name }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} is already in db" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} is not a playwright" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    output = []
+    Play.all.each { |play| output << play if play.playwright_id == @id}
+    output
+  end
 end
